3b9bdcfb825d2fa434a4138c02681d96839cd94d
Danillo Soares <danillohss@gmail.com>
2023-04-24 11:55:55 -0300

    directives-vuejs

--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const Vue = createApp(App)
+Vue.directive('texto', {
+    created(el, binding) {
+        console.log(binding.value)
+        if (binding.value?.cor) el.style.color = binding.value.cor
+        if (binding.value?.tamanhoFonte) el.style.fontSize = binding.value.tamanhoFonte
+
+        let totalCaracteres = 25;
+        if (binding.value?.totalCaracteres) totalCaracteres = binding.value.totalCaracteres
+
+        let originalText = el.innerText
+        let textLength = originalText;
+        let newText = ''
+        if (textLength.length > totalCaracteres) {
+            newText = originalText.substring(0, (totalCaracteres - 3)) + '...';
+        } else {
+            newText = originalText;
+        }
+        el.innerText = newText;
+    }
+})
+
+Vue.mount('#app')
+
+
+//createApp(App).mount('#app')
